debf7619854f888afdfce73cdf760a0352584aaf
OMBRENORMAL <134549221+samibumbar@users.noreply.github.com>
2024-08-19 16:36:59 +0200

    tema-9

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,36 @@
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+let colorInterval = null;
 
+// Centrează butoanele pe ecran
+const centerButtons = () => {
+  const buttonsContainer = startButton.parentElement;
+  buttonsContainer.style.display = 'flex';
+  buttonsContainer.style.justifyContent = 'center';
+  buttonsContainer.style.alignItems = 'center';
+  buttonsContainer.style.height = '100vh'; // Înălțimea ocupă întreaga fereastră
+  buttonsContainer.style.flexDirection = 'column'; // Butoanele sunt aranjate vertical
+};
+
+centerButtons();
+
+startButton.addEventListener('click', () => {
+  startButton.disabled = true;
+  stopButton.disabled = false;
+
+  colorInterval = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+});
+
+stopButton.addEventListener('click', () => {
+  clearInterval(colorInterval);
+  startButton.disabled = false;
+  stopButton.disabled = true;
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, '0')}`;
+}
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,36 @@
+import Notiflix from 'notiflix';
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        // Fulfill
+        resolve({ position, delay });
+      } else {
+        // Reject
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
+document.querySelector('.form').addEventListener('submit', event => {
+  event.preventDefault();
+  const delay = parseInt(event.target.delay.value);
+  const step = parseInt(event.target.step.value);
+  const amount = parseInt(event.target.amount.value);
+  console.log(delay, step, amount);
+  for (let i = 0; i < amount; i++) {
+    const currentDelay = delay + i * step;
+    createPromise(1, currentDelay)
+      .then(value => {
+        Notiflix.Notify.success(
+          `Fulfilled promise ${value.position} in ${value.delay}ms`
+        );
+      })
+      .catch(error => {
+        Notiflix.Notify.failure(
+          `Rejected promise ${error.position} in ${error.delay}ms`
+        );
+      });
+  }
+});
